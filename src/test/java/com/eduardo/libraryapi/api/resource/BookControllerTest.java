b338a6f345635ef191f191426a91ba142ab87796
NoteEdu <eduardo.tsilva@hotmail.com>
2021-04-23 19:20:16 -0400

    32. Aprendendo a desenhar uma requisição utilizando TDD

--- src/test/java/com/eduardo/libraryapi/api/resource/BookControllerTest.java
+++ src/test/java/com/eduardo/libraryapi/api/resource/BookControllerTest.java
@@ -1,14 +1,22 @@
 package com.eduardo.libraryapi.api.resource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -16,12 +24,33 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class BookControllerTest {
 
+    static String BOOK_API = "/api/books";
+
     @Autowired
     MockMvc mvc;
 
     @Test
     @DisplayName("DEVE criar uma livro com sucesso.")
-    public void createBookTest(){
+
+    public void createBookTest() throws Exception{
+
+        String json = new ObjectMapper().writeValueAsString(null);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc
+                .perform(request)
+                .andExpect(status().isCreated() )
+                .andExpect(jsonPath("id").isNotEmpty())
+                .andExpect( jsonPath("title").value("Meu Livro") )
+                .andExpect(jsonPath("author").value("Autor"))
+                .andExpect(jsonPath("isbn").value("123123"))
+
+                ;
 
     }
 
