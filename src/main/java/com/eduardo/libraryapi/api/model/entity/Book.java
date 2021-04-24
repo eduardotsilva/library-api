20c4e381b2feb6004e5730655a52d218a697c343
NoteEdu <eduardo.tsilva@hotmail.com>
2021-04-24 10:55:20 -0400

    43. Verificando a existência de um livro pelo ISBN na base de dados

--- src/main/java/com/eduardo/libraryapi/api/model/entity/Book.java
+++ src/main/java/com/eduardo/libraryapi/api/model/entity/Book.java
@@ -5,12 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table
 public class Book {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String author;
--- src/test/java/com/eduardo/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/com/eduardo/libraryapi/model/repository/BookRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.eduardo.libraryapi.model.repository;
 
+import com.eduardo.libraryapi.api.model.entity.Book;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,10 +24,13 @@ public class BookRepositoryTest {
     BookRepository repository;
 
     @Test
-    @DisplayName("DEVE retornar verdadeiro quando existir um livro na base com ibs informado")
+    @DisplayName("DEVE retornar TRUE quando existir um livro na base com ibs informado")
     public void returnTrueWhenIsbnExists(){
         //cenario
         String isbn  = "123";
+
+        Book book = Book.builder().title("Aventuras").author("Fulano").isbn(isbn).build();
+        testEntityManager.persist(book);
 
         //execucao
           boolean exists = repository.existsByIsbn(isbn);
@@ -34,4 +38,19 @@ public class BookRepositoryTest {
         //verificacao
         assertThat(exists).isTrue();
     }
+
+
+    @Test
+    @DisplayName("DEVE retornar FALSE quando não existir um livro na base com ibs informado")
+    public void returnFalseWhenIsbnDoesntExists(){
+        //cenario
+        String isbn  = "123";
+
+        //execucao
+        boolean exists = repository.existsByIsbn(isbn);
+
+        //verificacao
+        assertThat(exists).isFalse();
+    }
+
 }
