32154a4b9ac08cb5abb2e72f1aed1c03bb890e15
NoteEdu <eduardo.tsilva@hotmail.com>
2021-04-23 20:52:43 -0400

    37. Criando o teste da camada de serviço

---
+++ src/test/java/com/eduardo/libraryapi/service/BookServiceTest.java
@@ -0,0 +1,35 @@
+package com.eduardo.libraryapi.service;
+
+import com.eduardo.libraryapi.api.model.entity.Book;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public class BookServiceTest {
+
+    BookService service;
+
+    @Test
+    @DisplayName("DEVE salvar um livro")
+    public void saveBookTest(){
+
+        //cenario
+        Book book = Book.builder().isbn("123").author("Fulano").title("as aventuras").build();
+
+        //execucao
+        Book savedBook = service.save(book);
+
+        //verificação
+        assertThat(savedBook.getId()).isNotNull();
+        assertThat(savedBook.getIsbn()).isEqualTo("123");
+        assertThat(savedBook.getTitle()).isEqualTo("as aventuras");
+        assertThat(savedBook.getAuthor()).isEqualTo("Fulano");
+    }
+
+}
