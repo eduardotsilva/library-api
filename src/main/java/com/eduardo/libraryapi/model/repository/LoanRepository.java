bd1e6e83b057d6785508fe41cbba13b316370bc1
NoteEdu <eduardo.tsilva@hotmail.com>
2021-04-27 23:16:29 -0300

    76. Finalizando os testes de integração de empréstimos

--- src/main/java/com/eduardo/libraryapi/model/repository/LoanRepository.java
+++ src/main/java/com/eduardo/libraryapi/model/repository/LoanRepository.java
@@ -15,5 +15,9 @@ public interface LoanRepository extends JpaRepository<Loan, Long> {
             + "from Loan l where l.book = :book  and (l.returned is null or l.returned is false)")
     boolean existsByBookAndNotReturned(@Param("book") Book book);
 
-    Page<Loan> findByBookIsbnOrCustomer(String isbn, String customer, Pageable pageRequest);
+    @Query(value = "select l from Loan as l join l.book as b where b.isbn = :isbn or l.customer = :customer")
+    Page<Loan> findByBookIsbnOrCustomer(
+            @Param("isbn") String isbn,
+            @Param("customer")String customer,
+            Pageable pageable);
 }
--- src/test/java/com/eduardo/libraryapi/model/repository/LoanRepositoryTest.java
+++ src/test/java/com/eduardo/libraryapi/model/repository/LoanRepositoryTest.java
@@ -9,10 +9,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static com.eduardo.libraryapi.model.repository.BookRepositoryTest.createNewBook;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -33,23 +36,48 @@ public class LoanRepositoryTest {
     public void existsByBookAndNotReturnedTest(){
 
         //cenário
+        Loan loan = createAndPersisteLoan();
+        Book book = loan.getBook();
+
+        //execução
+        boolean exists = repository.existsByBookAndNotReturned(book);
+        assertThat(exists).isTrue();
+    }
+
+
+    @Test
+    @DisplayName("DEVE buscar empréstimo pelo isbn do livro ou customer")
+    public void findByBookIsbnOrCustomerTest(){
+        //cenário
+        Loan loan = createAndPersisteLoan();
+        Book book = loan.getBook();
+
+        Page<Loan> result = repository.findByBookIsbnOrCustomer(book.getIsbn(), loan.getCustomer(), PageRequest.of(0, 10));
+
+        List<Loan> content = result.getContent();
+
+        assertThat(content.size()).isEqualTo(1);
+        assertThat(content.contains(loan)).isTrue();
+        assertThat(result.getPageable().getPageSize()).isEqualTo(10);
+        assertThat(result.getPageable().getPageNumber()).isEqualTo(0);
+        assertThat(result.getTotalElements()).isEqualTo(1);
+
+    }
+
+    public Loan createAndPersisteLoan(){
+        //cenário
         Book book = createNewBook("123");
         entityManager.persist(book);
 
         Loan loan = Loan
-                    .builder()
-                    .book(book)
-                    .customer("Fulano")
-                    .loanDate(LocalDate.now())
-                    .build();
+                .builder()
+                .book(book)
+                .customer("Fulano")
+                .loanDate(LocalDate.now())
+                .build();
 
         entityManager.persist(loan);
-
-        //execução
-        boolean exists = repository.existsByBookAndNotReturned(book);
-
-        assertThat(exists).isTrue();
+        return loan;
     }
-
 
 }
