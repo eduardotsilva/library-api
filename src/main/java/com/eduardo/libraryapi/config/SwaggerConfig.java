2665d58def8dbffbb0c775e1f457751872f7fd26
NoteEdu <eduardo.tsilva@hotmail.com>
2021-05-01 19:13:03 -0300

    90.  Documento a API com swagger

---
+++ src/main/java/com/eduardo/libraryapi/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.eduardo.libraryapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.eduardo.libraryapi.api.resource"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Library API")
+                .description("API do Projeto de controle de aluguel de livros")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact() {
+        return new Contact("Eduardo Tadeu", "https://github.com/eduardotsilva", "eduardo.tsilva@hotmail.com");
+    }
+}
