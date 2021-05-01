189a81a0c6a2392d317014d3d2fca983a27d232c
NoteEdu <eduardo.tsilva@hotmail.com>
2021-05-01 13:11:02 -0300

    86. Testando o envio de emails

--- src/main/java/com/eduardo/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/eduardo/libraryapi/LibraryApiApplication.java
@@ -1,22 +1,41 @@
 package com.eduardo.libraryapi;
 
+import com.eduardo.libraryapi.service.EmailService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 @EnableScheduling
 public class LibraryApiApplication {
-	@Bean
-	public ModelMapper modelMapper(){
-		return new ModelMapper();
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryApiApplication.class, args);
-	}
+
+    @Autowired
+    private EmailService emailService;
+
+    @Bean
+    public CommandLineRunner runner() {
+        return args -> {
+            List<String> emails = Arrays.asList("library-api-144076@inbox.mailtrap.io");
+            emailService.sendMails("Testando serviço de emails", emails);
+            System.out.println("EMAILS ENVIADOS");
+        };
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApiApplication.class, args);
+    }
 
 
 }
