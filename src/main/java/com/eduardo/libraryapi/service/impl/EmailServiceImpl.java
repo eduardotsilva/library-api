c9c6d566d5c7879d6ca8146170dc2ff47e6c5767
NoteEdu <eduardo.tsilva@hotmail.com>
2021-05-01 12:34:17 -0300

    84. Configurando o servidor MailTrap para envio de emails

---
+++ src/main/java/com/eduardo/libraryapi/service/impl/EmailServiceImpl.java
@@ -0,0 +1,13 @@
+package com.eduardo.libraryapi.service.impl;
+
+import com.eduardo.libraryapi.service.EmailService;
+
+import java.util.List;
+
+public class EmailServiceImpl implements EmailService {
+
+    @Override
+    public void sendMails(String message, List<String> emailList) {
+
+    }
+}
